10ed3b3f1125af6aa98fcdd454eac42c975e78ce
gitName <a@b.c>
2023-10-22 01:30:03 +0100

    added google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import "@/styles/tailwind.css";
 import { Providers } from "./providers";
 import { cx } from "@/utils/all";
 import { Inter, Lora } from "next/font/google";
+import Script from "next/script";
+import * as gtag from '@/lib/gtag';
 
 const inter = Inter({
   subsets: ["latin"],
@@ -24,7 +26,23 @@ export default function RootLayout({
       suppressHydrationWarning
       className={cx(inter.variable, lora.variable)}>
       <body className="antialiased text-gray-800 dark:bg-black dark:text-gray-400">
-        <Providers>{children}</Providers>
+        <main><Providers>{children}</Providers></main>
+        <Script id="google-tag-script-id-1"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${gtag.GA_TRACKING_ID}', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
+        <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+        />
       </body>
     </html>
   );
